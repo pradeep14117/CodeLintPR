6b511483a21c35ff25b4dcde0fb0c6d9092e0ff4
halesh12345 <nm1397000@gmail.com>
2025-04-09 12:34:27 +0530

    Update manage.py

--- django_app/manage.py
+++ django_app/manage.py
@@ -3,14 +3,14 @@
 import os
 import sys
 
-# Import monkey patch first
+# Import monkey patches first
 import monkey  # Add this line
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
     try:
-        from django.core.management import execute_from_command_line
+        from djajjngo.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
